16aa9503ac1aec3317fc6300e3638a27cb875ea3
SAMBRANO <joao.sambrano@atos.net>
2021-10-25 21:55:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace Aula_25_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i);
+
+            //    i++;
+            //}
+
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+
+            //} while (i > 10);
+            //
+
+            //EXER1
+
+            //int i = 0;
+            //int somatoria = 0;
+            //while (i <= 100)
+            //{
+            //    somatoria = somatoria + i;            
+            //    i++;
+            //}
+            //Console.WriteLine(somatoria);
+
+            //exerc 2
+
+            //int i = 0,count = 0;
+            //double media, somatoria = 0;
+            //while (i <= 100)
+
+            //{
+            //    if (i % 2 != 0) ;
+            //    {
+            //        somatoria = somatoria + i;
+            //        count++;
+            //    }
+            //    i++;
+            //}
+
+            //Console.WriteLine(somatoria);
+            //media = somatoria / count;
+            //Console.WriteLine(media);
+
+            //exerc 3
+
+            //int i=0, v1, v2, multiplicacão = 0;
+
+            //Console.WriteLine("insira dois numeros: ");
+            //v1 = int.Parse(Console.ReadLine());
+            //v2 = int.Parse(Console.ReadLine());
+
+            //while (i < v1);
+            //{
+            //    multiplicacão += v2;
+            //    i++;
+            //}
+            //Console.WriteLine(multiplicacão);
+
+            //exer 4
+
+            //int i = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("Numero: ");
+            //    i = int.Parse(Console.ReadLine());
+
+            //    if(i % 2 == 0)
+            //    {
+            //        Console.WriteLine("PAR ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write(" IMPAR ");
+
+            //    }
+
+            //} while (i != 0);
+
+
+
+
+            //exercicio 5
+
+            //int i, m, d, soma, p;
+            //for(i=1000; i <= 9999; i++)
+            //{
+            //    m = i / 100;
+            //    d = i % 100;
+            //    soma = d+m;
+            //    p = soma * soma;
+
+            //    if(p==i)
+            //    {
+            //        Console.WriteLine(i+ "Regras ok.  ");
+            //    }
+
+            //}
+
+            ////exer 6
+
+            //int n, i;
+            //double s=0;
+
+
+            //Console.WriteLine("digite um numero");
+            //n = int.Parse(Console.ReadLine());
+            //for (i = 1; i <= n ; i++)
+            //{
+
+               
+            //    s += 1 / (double) i; 
+      
+            //    Console.WriteLine("O resultado: " + s);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
